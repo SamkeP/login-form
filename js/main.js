dd4ff7797353673dc4c60499c8c29cf9133529d2
SamkeP <samukeliswehadebe@gmail.com>
2021-08-19 16:55:20 +0200

    updating css, html and javascript

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
+ const input = document.querySelectorAll('.input');
+
+ function focusFunc(){
+     let parent = this.parentNode.parentNode;
+     parent.classList.add('focus')
+ }
+
+function blurFunc(){
+    let parent = this.parentNode.parentNode;
+    if(this.value ==""){
+        parent.classList.remove('focus');
+    }
+}
  
+
+
+inputs.forEach(input=> {
+     input.addEventListerner('focus',focusFunc);
+     input.addEventListerner('blur',blurFunc);
+     
+ });
